eab8b3b86d86670242abb51cffb5da6771800741
eddie Hinojosa <eddie_hinojosa@live.com>
2024-03-27 13:48:40 -0400

    added iamges folder and adjusted the placement of the ligtmode switcher

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,22 +1,36 @@
 // this JS section is for the light/dark mode toggle button
 
 //Define variables
-const displayChange = document.getElementById("#displayChange")
-const lightmode = document.querySelector("#lightmode")
+const displayChange = document.getElementById("#displayChange");
+const lightmode = document.querySelector("#lightmode");
 
 var mode = 'light'; 
 
 
-
 //Event change upon button click
-// displayChange.addEventListener('click', function(){
-//     if (mode === 'light'){
-//         mode = 'dark';
-//         lightmode.className = 'darkmode';}
-//     } else {
-//         mode = "light";
-//     }
-// )
+displayChange.addEventListener('click', function(){
+    if (mode === 'light'){
+        mode = 'dark';
+        lightmode.className = 'darkmode';
+    } else {
+        mode = 'light'
+        lightmode.className = 'lightmode';
+    }
+});
+
+
+
+//testing 2 separate functions for comor theme change rathern than an if/else statement
+// displayChange.addEventListener('click', myFunction() {
+//     var element = document.body.header.h1;
+//     element.classList.toggle("darkmode");
+    
+// })
+
+
+
+
+
 
 
 
