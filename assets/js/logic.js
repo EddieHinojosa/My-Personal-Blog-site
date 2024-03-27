b5761e230cec1a2aaa37fbba2f7ce6e35fe0c599
eddie Hinojosa <eddie_hinojosa@live.com>
2024-03-27 12:23:33 -0400

    updating light/dark mode js functions

---
+++ assets/js/logic.js
@@ -0,0 +1,24 @@
+// this JS section is for the light/dark mode toggle button
+
+//Define variables
+const displayChange = document.getElementById("#displayChange")
+const lightmode = document.querySelector("#lightmode")
+
+var mode = 'light'; 
+
+
+
+//Event change upon button click
+// displayChange.addEventListener('click', function(){
+//     if (mode === 'light'){
+//         mode = 'dark';
+//         lightmode.className = 'darkmode';}
+//     } else {
+//         mode = "light";
+//     }
+// )
+
+
+
+
+
