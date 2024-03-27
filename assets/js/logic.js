05677d735fc767cd8c055fd3c99fb75a61c67c16
eddie Hinojosa <eddie_hinojosa@live.com>
2024-03-27 17:54:59 -0400

    fix teh syntaxt errors on the js if/else

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,26 +1,29 @@
 // this JS section is for the light/dark mode toggle button
 
-//Define variables
-const displayChange = document.getElementById("#displayChange");
-const lightmode = document.querySelector("#lightmode");
+    //Define variables
 
-var mode = 'light'; 
+const lightmode = document.querySelector('.lightmode');
+const displayChange = document.getElementById('displayChange');
+
+let theme = 'light'; 
 
 
 //Event change upon button click
-displayChange.addEventListener('click', function(){
-    if (mode === 'light'){
-        mode = 'dark';
-        lightmode.className = 'darkmode';
+displayChange.addEventListener('click', function() {
+    if (theme === 'light') {
+        theme = 'dark';
+        lightmode.className = 'dark';
+        console.log('lets go darkmode');
     } else {
-        mode = 'light'
-        lightmode.className = 'lightmode';
+        theme = 'light'
+        lightmode.className= 'light';
+        console.log('lets go lightmode');
     }
 });
 
 
 
-//testing 2 separate functions for comor theme change rathern than an if/else statement
+// testing 2 separate functions for comor theme change rathern than an if/else statement
 // displayChange.addEventListener('click', myFunction() {
 //     var element = document.body.header.h1;
 //     element.classList.toggle("darkmode");
